ccc95c9f76865c95ffb9e0fb5c226fae6f85fbb5
Tiffany <tiff.sobczak@gmail.com>
2021-12-19 14:58:48 -0600

    added functional api calls with limiting the array - added some bootstrap styling

--- assets/script.js
+++ assets/script.js
@@ -10,8 +10,8 @@ var formSubmitHandler = function(event) {
     var cityName = nameInputEl.value.trim();
 
     if (cityName) {
-        getCityCond(cityName);
-
+        getCityCoordinates(cityName)
+    
         //clear old content
         currentCondEl.textContent='';
         nameInputEl.value='';
@@ -19,20 +19,30 @@ var formSubmitHandler = function(event) {
         alert('Please enter a city name');
     }
 };
+var getCityCoordinates = function(city) {
+    var apiUrl= 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=d8a50e839877cad08e505ca47a0dd3aa'
+    fetch (apiUrl)
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                getCityConditions (data[0].lat, data[0].lon)
+            })
+        }
+    })
+}
 
-var getCityCond = function (city) {
+var getCityConditions = function (latitute, longitude) {
     //format the weather api url
-    var apiUrl= 'api.openweathermap.org/data/2.5/weather?q=' + cityName +'&appid=d8a50e839877cad08e505ca47a0dd3aa';
+    var apiUrl= 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitute + '&lon=' + longitude + '&appid=d8a50e839877cad08e505ca47a0dd3aa&exlude=hourly,daily,alerts&units=imperial';
 
     //make a request to url
     fetch (apiUrl)
     .then(function(response) {
         //request was successful
         if (response.ok) {
-            console.log (reponse);
             response.json().then(function(data) {
                 console.log(data);
-                displayConditions(data, city);
+                displayConditions(data.current, data.daily);
             });
         } else {
             alert('Error: ' + response.statusText);
@@ -43,13 +53,30 @@ var getCityCond = function (city) {
     });
 };
 
-var displayConditions = function(conditions, searchTerm) {
-    //check if api returned any conditions
-    if (conditions ===0) {
-        currentCondEl.textContent = "No conditions found.";
-        return;
+var displayConditions = function(current, forecast) {
+    
+    console.log(current, forecast)
+
+    let currentUviColor = 'green';
+
+    if (current.uvi > 2) {
+        currentUviColor = 'yellow';
     }
-    conditionSearchTerm.textContent = searchTerm;
+
+    if (current.uvi > 5) {
+        currentUviColor = 'red';
+    }
+
+    currentCondEl.innerHTML=`
+    <div class="">
+        <div>Temperature: ${current.temp}</div>
+        <div>Wind: ${current.wind_speed}</div>
+        <div>Humidity: ${current.humidity}</div>
+        <div style="background-color: ${currentUviColor}">UV Index: ${current.uvi}</div>
+        
+    </div>
+    `
+    
 
 }
 
